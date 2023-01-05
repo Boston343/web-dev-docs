f6a16fe043b426bca0734cbc90d029f89eaab248
Boston343 <branden.austin1@gmail.com>
2023-01-04 23:29:19 -0500

    after much pain, I have succeeded in switching the material UI theme based on the Docusaurus them... and being able to change what each of those themes look like using the experimental css vars provider from MUI

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,20 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import HomepageCards from "@site/src/components/HomepageCards";
+import {
+  Experimental_CssVarsProvider as CssVarsProvider,
+  experimental_extendTheme as extendTheme,
+} from "@mui/material/styles";
+import { useColorMode } from "@docusaurus/theme-common";
 
 import styles from "./index.module.css";
 import { Button } from "@mui/material";
 
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+import {
+  ThemeProvider,
+  createTheme,
+  useColorScheme,
+} from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
@@ -21,13 +30,53 @@ const theme = createTheme({
       main: "#fff",
     },
     background: {
-      // paper: "hsl(240, 90%, 70%)",
+      paper: "hsl(240, 90%, 70%)",
+      // paper: "var(--mui-palette-background-paper)",
+    },
+  },
+});
+
+const exTheme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: {
+          main: "hsl(240, 48%, 47%)",
+        },
+        secondary: {
+          main: "#fff",
+        },
+        background: {
+          // paper: "hsl(240, 48%, 47%)",
+        },
+      },
+    },
+    dark: {
+      palette: {
+        primary: {
+          main: "hsl(240, 90%, 70%)",
+        },
+        secondary: {
+          main: "#fff",
+        },
+        background: {
+          // paper: "hsl(240, 90%, 70%)",
+        },
+      },
     },
   },
 });
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
+
+  // get the Docusaurus theme
+  const { isDarkTheme } = useColorMode();
+
+  // set Material UI theme based on Docusaurus theme
+  const { setMode } = useColorScheme();
+  setMode(isDarkTheme ? "dark" : "light");
+
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
@@ -42,7 +91,7 @@ function HomepageHeader() {
             href="/docs/intro"
             color="secondary"
           >
-            Docs
+            Docs {isDarkTheme ? "Dark" : "Light"}
           </Button>
         </div>
       </div>
@@ -74,7 +123,8 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <>
-      <ThemeProvider theme={theme}>
+      {/* <ThemeProvider theme={theme}> */}
+      <CssVarsProvider theme={exTheme}>
         <Layout
           title={`Reap3r ${siteConfig.title}`}
           description="Documentation on web development tips, tools, and techniques. By Reap3r"
@@ -86,7 +136,8 @@ export default function Home() {
             <HomepageCards />
           </main>
         </Layout>
-      </ThemeProvider>
+      </CssVarsProvider>
+      {/* </ThemeProvider> */}
     </>
   );
 }
